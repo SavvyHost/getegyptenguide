6b38bf75120c327d16a754f5488172ab16e26b05
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-11-16 04:52:52 +0200

    commit

--- src/components/molecules/Blogs/HeroBlog.tsx
+++ src/components/molecules/Blogs/HeroBlog.tsx
@@ -4,7 +4,7 @@ import Blogs from "../../../../public/assets/bgblogs.png";
 
 const HeroBlog = () => {
   return (
-    <div className="relative w-full h-[50vh] md:h-[90vh]">
+    <div className="relative w-full h-[40vh] md:h-[50vh]">
       <Image
         src={Blogs}
         alt="Background"
--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -32,23 +32,20 @@ type Props = {
 const BLogs: React.FC<Props> = ({ blogData, Destinations }) => {
   const limitedDestinations = Destinations.slice(0, 8);
   return (
-    <div className="bg-[#FAFAFA] lg:px-16 mt-16 pt-3 px-4 py-3">
-      {/* <HeroBlog /> */}
+    <div className="bg-[#FAFAFA]  mt-12 pt-3 py-3">
+      <HeroBlog />
       {/* <InterestsSection /> */}
       {/* <div className="lg:px-16 p-4 bg-[#FAFAFA] ">
         <DestinationSection Destinations={limitedDestinations} />
       </div> */}
 
       <div className=" ">
-        <div className="text-left text-black font-segoe sm:font-semi-bold font-medium text-2xl md:text-special-offer mb-4">
+        {/* <div className="text-left text-black font-segoe sm:font-semi-bold font-medium text-2xl md:text-special-offer mb-4">
           Recent Blogs
         </div>
-        <RecentBlog />
+        <RecentBlog /> */}
       </div>
-      <div className="mt-4">
-        <div className="text-left text-black font-segoe sm:font-semi-bold font-medium text-2xl md:text-special-offer mb-4">
-          Blogs
-        </div>
+      <div className="mt-4 lg:px-16  px-4">
         <BlogSection blogData={blogData} />
       </div>
     </div>
