481ba949c8fe1be9db73a225dc8f59cafd5fce6c
elkhamyyali <elkhamyyalimohamedali@gmail.com>
2024-12-15 20:13:00 +0200

    commit

--- src/components/organisms/HeroSection.tsx
+++ src/components/organisms/HeroSection.tsx
@@ -5,12 +5,12 @@ import TwoMainButtons from "../atoms/Form/TwoMainButtons";
 
 const HeroSection = () => {
   return (
-    <section className="relative w-full h-screen overflow-hidden">
+    <section className="relative w-full h-[95vh] overflow-hidden">
       {/* Video Background */}
       <div className="relative w-full h-0" style={{ paddingBottom: "56.25%" }}>
         <iframe
           className="absolute top-0 left-0 w-full h-full"
-          src="https://www.youtube.com/embed/mfxQy5A_tHs?autoplay=1&controls=0&showinfo=0&rel=0&mute=1"
+          src="https://www.youtube.com/embed/dz8qoMeNlBs?autoplay=1&controls=0&showinfo=0&rel=0&mute=1&loop=1&playlist=dz8qoMeNlBs"
           title="YouTube video player"
           frameBorder="0"
           allow="autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -18,9 +18,11 @@ const HeroSection = () => {
         ></iframe>
       </div>
 
-      {/* Form Above Video */}
-      {/* <BluerForm /> */}
-      <TwoMainButtons />
+      {/* Content Above Video */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
+        {/* <BluerForm /> */}
+        <TwoMainButtons />
+      </div>
     </section>
   );
 };
